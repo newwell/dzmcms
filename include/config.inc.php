0e58aa93b0b6e3073b64b9219f0a430cd36071b9
newwell <hubei_java@qq.com>
2013-07-17 09:11:41 +0800

    编码方式修改了一下

--- include/config.inc.php
+++ include/config.inc.php
@@ -8,13 +8,13 @@ $db_host	= '127.0.0.1';
 $db_user	= 'root';
 
 //数据库密码
-$db_pass	= '';
+$db_pass	= 'root';
 
 //数据库名称
-$db_name	= 'durl';
+$db_name	= 'dzmcms';
 
 //数据表前缀
-$tablepre	= 'durl_';
+$tablepre	= 'dzmc_';
 
 //程序调试模式,设定为true为开启，false关闭
 $enable_debugmode	= true;
